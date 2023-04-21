a583fe5f61378a8be70a372736a3ec7c7a3586e0
Emil Anton <emilbstn@gmail.com>
2023-04-20 21:09:32 -0700

    add third test to verify functionality of LoafCost method

--- Products.Tests/ModelTests/Products.Tests.cs
+++ Products.Tests/ModelTests/Products.Tests.cs
@@ -20,5 +20,12 @@ namespace Products.Tests
       New.LoafCost(3);
       Assert.AreEqual(10, New.OrderCost);
     }
+    [TestMethod]
+    public void IsBreadCost_Twenty_True()
+    {
+      OrderBread New = new OrderBread(6);
+      New.LoafCost(6);
+      Assert.AreEqual(20, New.OrderCost);
+    }
   }
 }
