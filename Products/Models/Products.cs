96a0e12d1d1bf87aecda6be574e4e1a595b8b396
Emil Anton <emilbstn@gmail.com>
2023-04-20 20:56:30 -0700

    update LoafCost with if/else statements to check every third loaf ordered to be free

--- Products/Models/Products.cs
+++ Products/Models/Products.cs
@@ -5,11 +5,31 @@ namespace PierresBakery.Models
     public string FoodNumber {get; set;}
     public int IndivCost = 5;
     public int OrderCost = 0;
+    public int FreeNumber = 3;
     public OrderBread (int FoodNumber)
     {}
     public void LoafCost(int FoodNumber)
     {
-      OrderCost = IndivCost * FoodNumber;
+      if ((FoodNumber) % FreeNumber == 0)
+      {
+        int FoodNumTrue = (FoodNumber-(FoodNumber/FreeNumber));
+        OrderCost = FoodNumTrue * IndivCost;
+      }
+      else if (((FoodNumber) % FreeNumber != 0)&&(FoodNumber > FreeNumber))
+      {
+        int FoodExtra = 0;
+        while (FoodExtra % FreeNumber != 0)
+        {
+          FoodExtra++;
+          FoodNumber--;
+        }
+        int FoodNumTrue = (FoodNumber-(FoodNumber/FreeNumber));
+        OrderCost = (FoodNumTrue + FoodExtra) * IndivCost;
+      }
+      else 
+      {
+        OrderCost = FoodNumber * IndivCost;
+      }
     }
   }
 }
