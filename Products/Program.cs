0aee2162e9a595fe72f0fb28858650c4d672c267
Emil Anton <emilbstn@gmail.com>
2023-04-20 21:39:31 -0700

    add console statements to take and process user's chosen number of loaves they want to order and returns the cost

--- Products/Program.cs
+++ Products/Program.cs
@@ -1,6 +1,17 @@
-class Program
+using PierresBakery.Models;
+using System;
+namespace PierresBakery
 {
-  static void Main()
+  public class Program
+{
+  public static void Main()
   {
+    Console.WriteLine("We sell Bread by the loaf, how many would you like?");
+    int BreadNumber = Int32.Parse(Console.ReadLine());
+    OrderBread NewBread = new OrderBread(BreadNumber);
+    NewBread.LoafCost(BreadNumber);
+    int BreadCost = NewBread.OrderCost;
+    Console.WriteLine("Your Order will cost " + BreadCost + " dollars.");
   }
+}
 }
