9d39d69848331d374c80d59f9f4bed41da43c304
Emil Anton <emilbstn@gmail.com>
2023-04-21 09:39:47 -0700

    add if statement to check if the input from user is a number and not written words

--- Products/Program.cs
+++ Products/Program.cs
@@ -3,15 +3,23 @@ using System;
 namespace PierresBakery
 {
   public class Program
-{
-  public static void Main()
   {
-    Console.WriteLine("We sell Bread by the loaf, how many would you like?");
-    int BreadNumber = Int32.Parse(Console.ReadLine());
-    OrderBread NewBread = new OrderBread(BreadNumber);
-    NewBread.LoafCost(BreadNumber);
-    int BreadCost = NewBread.OrderCost;
-    Console.WriteLine("Your Order will cost " + BreadCost + " dollars.");
+    public static void Main()
+    {
+      Console.WriteLine("We sell Bread by the loaf, how many would you like? Please answer with a number and not letters.");
+      string BreadInput = Console.ReadLine();
+      int i = 0;
+      if (int.TryParse(BreadInput, out i) == true) {
+        int BreadNumber = Int32.Parse(BreadInput);
+        OrderBread NewBread = new OrderBread(BreadNumber);
+        NewBread.LoafCost(BreadNumber);
+        int BreadCost = NewBread.OrderCost;
+        Console.WriteLine("Your Order will cost " + BreadCost + " dollars.");
+      }
+      else 
+      {
+        Console.WriteLine("Sorry, our system can't process written answers, try again with a number value please.");
+      }
+    }
   }
-}
 }
