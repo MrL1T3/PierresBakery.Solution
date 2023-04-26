8eab5ff98f75dd88ed5e1fbdc00367dde3ca5ef1
Emil Anton <emilbstn@gmail.com>
2023-04-26 08:52:57 -0700

    add console funtions to display and take order of pastries

--- Products/Program.cs
+++ Products/Program.cs
@@ -6,15 +6,22 @@ namespace PierresBakery
   {
     public static void Main()
     {
-      Console.WriteLine("We sell Bread by the loaf, how many would you like? Please answer with a number and not letters.");
+      Console.WriteLine("Welcome to Pierre's Bakery! We sell Bread by the loaf and individual pastries");
+      Console.WriteLine("How many loaves of bread would you like? Please answer with a number and not letters.");
       string BreadInput = Console.ReadLine();
+      Console.WriteLine("And how many pastries would yo like?");
+      string PastryInput = Console.ReadLine();
       int i = 0;
-      if (int.TryParse(BreadInput, out i) == true) {
+      if (int.TryParse(BreadInput, out i) == true && int.TryParse(PastryInput, out i) == true) {
         int BreadNumber = Int32.Parse(BreadInput);
+        int PastryNumber = Int32.Parse(PastryInput);
         OrderBread NewBread = new OrderBread(BreadNumber);
+        OrderPastry NewPastry = new OrderPastry(PastryNumber);
         NewBread.LoafCost(BreadNumber);
-        int BreadCost = NewBread.OrderCost;
-        Console.WriteLine("Your Order will cost " + BreadCost + " dollars.");
+        NewPastry.TartCost(PastryNumber);
+        int BreadCost = NewPastry.OrderCost;
+        int PastryCost = NewBread.OrderCost;
+        Console.WriteLine("Your Order will cost " + (BreadCost + PastryCost) + " dollars.");
       }
       else 
       {
